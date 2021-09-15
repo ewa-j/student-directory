ab4a3033e659d965a0551809ddd5af98110001b3
ewa <ewa.jablonskaa@gmail.com>
2021-09-15 12:06:07 +0100

    directory.rb added

---
+++ directory.rb
@@ -0,0 +1,19 @@
+#printing out the list of students
+puts "The students of Villains Academy"
+puts "--------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+#printing out the overall number of students using print command
+#unlike puts, print doesn't add newline at the end
+print "Overall, we have "
+print 11
+puts " great students"
